642551b92f19a4d01399dbf050409589adb55c00
leomordasini <leonardo.mordasini@gmail.com>
2021-11-12 13:29:47 -0500

    fixed tfvars typo

--- script.sh
+++ script.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-print_message(){ 
-    "First step passed"
-}
-
 # this will install terraform in the cloud shell
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 mkdir ~/bin
@@ -11,23 +7,11 @@ ln -s ~/.tfenv/bin/* ~/bin/
 tfenv install latest
 tfenv use latest
 
-print_message(){ 
-    "Second step passed"
-}
-
 # this will create a terraform.tfvars file for cloud shell
-cat > provider.tf <<EOF
+cat > terraform.tfvars <<EOF
 region          = "us-east-2"
 cluster_name    = "my-eks-cluster"
 instance_type   = "t2.small"
 EOF
 
-print_message(){ 
-    "Third step passed"
-}
-
 terraform init
-
-print_message(){ 
-    "Fourth step passed"
-}
