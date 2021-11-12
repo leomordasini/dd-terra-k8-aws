c7445c1a15c5ae020ddeabd11fce41e376ea14c9
leomordasini <leonardo.mordasini@gmail.com>
2021-11-12 15:32:36 -0500

    Update kubectl_installation.sh

--- kubectl_installation.sh
+++ kubectl_installation.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# Install kubectl
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+# Retrieve the access credentials and automatically configure kubectl
+aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)
