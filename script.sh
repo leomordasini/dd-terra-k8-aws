599ac709899d4a70ef47d0b645001c08be6f9b53
leomordasini <leonardo.mordasini@gmail.com>
2021-11-12 13:17:48 -0500

    fixed version problem

--- script.sh
+++ script.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
+print "First step passed"
+
 # this will install terraform in the cloud shell
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 mkdir ~/bin
 ln -s ~/.tfenv/bin/* ~/bin/
-TFENV_AUTO_INSTALL=false terraform plan
 tfenv install
+tfenv use <version>
+
+print "Second step passed"
 
 # this will create a terraform.tfvars file for cloud shell
 cat > provider.tf <<EOF
@@ -14,4 +18,8 @@ cluster_name    = "my-eks-cluster"
 instance_type   = "t2.small"
 EOF
 
+print "Third step passed"
+
 terraform init
+
+print "Final step passed"
