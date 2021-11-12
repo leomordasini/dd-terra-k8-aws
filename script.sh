5a4f84ea391447604d27ccf703c26b299674fcc0
leomordasini <leonardo.mordasini@gmail.com>
2021-11-12 13:24:14 -0500

    updated version to latest

--- script.sh
+++ script.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 
-print "First step passed"
+print_message(){ 
+    "First step passed"
+}
 
 # this will install terraform in the cloud shell
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 mkdir ~/bin
 ln -s ~/.tfenv/bin/* ~/bin/
-tfenv install
-tfenv use <version>
+tfenv install latest
+tfenv use latest
 
-print "Second step passed"
+print_message(){ 
+    "Second step passed"
+}
 
 # this will create a terraform.tfvars file for cloud shell
 cat > provider.tf <<EOF
@@ -18,8 +22,12 @@ cluster_name    = "my-eks-cluster"
 instance_type   = "t2.small"
 EOF
 
-print "Third step passed"
+print_message(){ 
+    "Third step passed"
+}
 
 terraform init
 
-print "Final step passed"
+print_message(){ 
+    "Fourth step passed"
+}
